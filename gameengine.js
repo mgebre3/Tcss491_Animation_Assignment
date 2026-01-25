f4e073e026f01436733d5c82055053f1b1d478e7
mgebre3 <mgebre3@uw.edu>
2026-01-25 10:38:54 -0800

    Update gameengine.js

--- gameengine.js
+++ gameengine.js
@@ -2,36 +2,61 @@ class GameEngine {
   constructor() {
     this.entities = [];
     this.keys = {};
+    this.clockTick = 0;
   }
 
   init(ctx) {
     this.ctx = ctx;
-    window.addEventListener("keydown", e => this.keys[e.key.toLowerCase()] = true);
-    window.addEventListener("keyup", e => this.keys[e.key.toLowerCase()] = false);
+
+    // Good for pixel/sprite graphics (prevents blur)
+    this.ctx.imageSmoothingEnabled = false;
+
+    // Keyboard input
+    window.addEventListener("keydown", (e) => {
+      this.keys[e.key.toLowerCase()] = true;
+    });
+
+    window.addEventListener("keyup", (e) => {
+      this.keys[e.key.toLowerCase()] = false;
+    });
+
     this.last = performance.now();
   }
 
-  addEntity(e) {
-    this.entities.push(e);
+  addEntity(entity) {
+    this.entities.push(entity);
   }
 
   start() {
     const loop = (t) => {
       this.clockTick = (t - this.last) / 1000;
       this.last = t;
+
       this.update();
       this.draw();
+
       requestAnimationFrame(loop);
     };
+
     requestAnimationFrame(loop);
   }
 
   update() {
-    this.entities.forEach(e => e.update());
+    for (const e of this.entities) {
+      e.update();
+    }
   }
 
   draw() {
-    this.ctx.clearRect(0,0,800,600);
-    this.entities.forEach(e => e.draw(this.ctx));
+    const w = this.ctx.canvas.width;
+    const h = this.ctx.canvas.height;
+
+    // Always draw a background so you know the canvas is working
+    this.ctx.fillStyle = "#eeeeee";
+    this.ctx.fillRect(0, 0, w, h);
+
+    for (const e of this.entities) {
+      e.draw(this.ctx);
+    }
   }
 }
